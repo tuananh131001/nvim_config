6d2c059e75ae7b2b431ab7e67b7dff4a3f783ac8
Anh Nguyen <anh.nt@josys.com>
2025-02-14 17:00:24 +0700

    Update plugins and modified diffview

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -18,6 +18,7 @@ return {
 			{ "<leader>opc", "<cmd>Octo pr create draft<CR>", desc = "Create pr in draft(Octo)" },
 			{ "<leader>opC", "<cmd>Octo pr create<CR>", desc = "Create pr (Octo)" },
 			{ "<leader>opb", "<cmd>Octo pr browser<CR>", desc = "Open PR in Browser(Octo)" },
+			{ "<leader>opm", "<cmd>Octo pr list tuananh131001<CR>", desc = "List of pr by my name(Octo)" },
 		},
 	},
 	{
@@ -138,6 +139,7 @@ return {
 			},
 		},
 		config = function()
+			local actions = require("diffview.actions")
 			require("diffview").setup({
 				view = {
 					default = {
@@ -151,6 +153,59 @@ return {
 					},
 					merge_tool = {
 						layout = "diff1_plain",
+					},
+				},
+
+				keymaps = {
+					view = {
+						{
+							"n",
+							"<leader>do",
+							actions.conflict_choose("ours"),
+							{ desc = "Choose the OURS version of a conflict" },
+						},
+						{
+							"n",
+							"<leader>dt",
+							actions.conflict_choose("theirs"),
+							{ desc = "Choose the THEIRS version of a conflict" },
+						},
+						{
+							"n",
+							"<leader>db",
+							actions.conflict_choose("base"),
+							{ desc = "Choose the BASE version of a conflict" },
+						},
+						{
+							"n",
+							"<leader>da",
+							actions.conflict_choose("all"),
+							{ desc = "Choose all the versions of a conflict" },
+						},
+						{
+							"n",
+							"<leader>dO",
+							actions.conflict_choose_all("ours"),
+							{ desc = "Choose the OURS version of a conflict for the whole file" },
+						},
+						{
+							"n",
+							"<leader>dT",
+							actions.conflict_choose_all("theirs"),
+							{ desc = "Choose the THEIRS version of a conflict for the whole file" },
+						},
+						{
+							"n",
+							"<leader>dB",
+							actions.conflict_choose_all("base"),
+							{ desc = "Choose the BASE version of a conflict for the whole file" },
+						},
+						{
+							"n",
+							"<leader>dA",
+							actions.conflict_choose_all("all"),
+							{ desc = "Choose all the versions of a conflict for the whole file" },
+						},
 					},
 				},
 			})
