891b1c8a68d7889c77b0f3eaca34fe85d10c7427
Anh Nguyen <anh.nt@josys.com>
2025-03-09 13:16:55 +0700

    feat(scope): octo nvim with snack picker

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -12,7 +12,7 @@ return {
 			enable_builtin = true,
 			default_to_projects_v2 = true,
 			default_merge_method = "squash",
-			picker = "fzf-lua",
+			picker = "snacks",
 		},
 		keys = {
 			{ "<leader>opc", "<cmd>Octo pr create draft<CR>", desc = "Create pr in draft(Octo)" },
