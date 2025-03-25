f0a1cc81c4a64397f2a8db47f1fc902c07e5b205
Anh Nguyen <anh.nt@josys.com>
2025-03-25 21:38:45 +0700

    feat(scope): vtsls cum back

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,63 +12,9 @@ return {
 					enabled = false,
 				},
 				vtsls = {
-					enabled = false,
+					enabled = true,
 				},
 			},
-		},
-	},
-	{
-		"pmizio/typescript-tools.nvim",
-		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-		event = "VeryLazy",
-		opts = {
-			settings = {
-				code_lens = "off", -- all
-				tsserver_file_preferences = {
-					includeInlayParameterNameHints = "all",
-					includeCompletionsForModuleExports = true,
-					quotePreference = "auto",
-				},
-				tsserver_format_options = {
-					allowIncompleteCompletions = false,
-					allowRenameOfImportPath = false,
-				},
-			},
-			-- Move on_attach inside opts
-			on_attach = function(_, bufnr)
-				vim.keymap.set(
-					"n",
-					"<leader>co",
-					"<cmd>TSToolsOrganizeImports<CR>",
-					{ buffer = bufnr, desc = "Typescript Organize Imports" }
-				)
-				vim.keymap.set(
-					"n",
-					"<leader>cM",
-					"<cmd>TSToolsAddMissingImports<CR>",
-					{ buffer = bufnr, desc = "Typescript Add Missing Imports" }
-				)
-				vim.keymap.set(
-					"n",
-					"<leader>cu",
-					"<cmd>TSToolsRemoveUnusedImports<CR>",
-					{ buffer = bufnr, desc = "Typescript Remove Unused Imports" }
-				)
-				-- vim.keymap.set(
-				--   "n",
-				--   "<leader>cR",
-				--   "<cmd>TSToolsRenameFile<CR>",
-				--   { buffer = bufnr, desc = "Typescript Rename File" }
-				-- )
-				-- vim.api.nvim_create_autocmd("BufWritePre", {
-				--   pattern = "*.ts,*.tsx,*.jsx,*.js",
-				--   callback = function(args)
-				--     vim.cmd("TSToolsAddMissingImports sync")
-				--     vim.cmd("TSToolsOrganizeImports sync")
-				--     require("conform").format({ bufnr = args.buf })
-				--   end,
-				-- })
-			end,
 		},
 	},
 }
