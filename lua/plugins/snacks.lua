460ec13d07883ac61f42bbe5a800f29ca25b6aa6
Anh Nguyen <anh.nt@josys.com>
2025-03-31 00:14:02 +0700

    feat: Setup vtsls

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,33 @@
+return {
+	{
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    explorer = { enabled = false },
+    indent = { enabled = true },
+    input = { enabled = true },
+    picker = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
+  keys = {
+	{ "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+	{ "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+	{ "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
+	{ "<leader>fR", function() Snacks.picker.recent({ filter = { cwd = true }}) end, desc = "Recent (cwd)" },
+	-- Grep
+	{ "<leader>sg", function() Snacks.picker.grep() end, desc = "Grep" },
+  }
+},
+}
