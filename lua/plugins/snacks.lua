5d9e7c01a102ba777513e9f65e16368ef3bf1c69
Anh Nguyen <anh.nt@josys.com>
2025-04-01 09:15:38 +0700

    feat(input): add snack input

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -19,6 +19,7 @@ return {
 			scroll = { enabled = true },
 			statuscolumn = { enabled = true },
 			words = { enabled = true },
+      input = { enabled = true }
 		},
     -- stylua: ignore
     keys = {
