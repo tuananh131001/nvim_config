27a8b97e576952c87191de25bad0fedbe4ae39ae
Anh Nguyen <anh.nt@raksul.com>
2025-10-08 12:59:58 +0700

    add new color pattern

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,19 +1,33 @@
 return {
- {
-    "OXY2DEV/markview.nvim",
-    lazy = false,
+	{
+		"OXY2DEV/markview.nvim",
+		lazy = false,
 
-   -- For `nvim-treesitter` users.
-    priority = 49,
+		-- For `nvim-treesitter` users.
+		priority = 49,
 
-    preview = {
-      icon_provider = "mini"
-    }
-
-    -- For blink.cmp's completion
-    -- source
-    -- dependencies = {
-    --     "saghen/blink.cmp"
-    -- },
-};
+		preview = {
+			icon_provider = "mini",
+		},
+		opts = {
+			preview = {
+				enable = false,
+			},
+		},
+		keys = {
+			{
+				"<leader>mt",
+				"<Cmd>Markview toggle<CR>",
+				desc = "toggle markdown preview",
+				mode = "n",
+				noremap = true,
+				silent = true,
+			},
+		},
+		-- For blink.cmp's completion
+		-- source
+		-- dependencies = {
+		--     "saghen/blink.cmp"
+		-- },
+	},
 }
