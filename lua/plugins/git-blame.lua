0cca999aeaa0c0cb59bc359669621e5ce6d523e3
Anh Nguyen <anh.nt@raksul.com>
2025-10-29 11:04:47 +0700

    Add git blame

--- lua/plugins/git-blame.lua
+++ lua/plugins/git-blame.lua
@@ -2,6 +2,9 @@ return {
 	{
 		"f-person/git-blame.nvim",
 		event = "VeryLazy",
+		keys = {
+			{"<leader>bd", "<Cmd>GitBlameToggle<CR>", mode = "n", noremap = true, silent = true, desc = "Toggle Git Blame",},
+		},
 		opts = {
 			enabled = true, -- if you want to enable the plugin
 			message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
