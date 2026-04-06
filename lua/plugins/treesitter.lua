57bde15a5a47d8ffaf4ec597ffb238830e060f78
Anh Nguyen <anh.nt@raksul.com>
2026-04-06 11:04:07 +0700

    Update tree-sitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,74 @@
-return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-    lazy = false,
-		event = { "BufReadPre", "BufNewFile" },
-		build = ":TSUpdate",
-    priority = 50,
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				auto_install = true,
-				indent = { enable = true },
-				highlight = { enable = true, additional_vim_regex_highlighting = false },
-				ensure_installed = { "php", "tsx", "typescript", "javascript", "html", "css" },
-			})
+local M = {
+	"nvim-treesitter/nvim-treesitter",
 
-			vim.treesitter.language.register("markdown", "octo")
-		end,
-	},
+	lazy = false,
+	branch = "main",
+	build = ":TSUpdate",
 }
+
+M.config = function()
+	local parsers = {
+		"bash",
+		-- 'c',
+		-- 'comment',
+		"css",
+		"csv",
+		"diff",
+		"dockerfile",
+		"gitignore",
+		-- 'go',
+		"html",
+		"javascript",
+		-- 'jsdoc',
+		"json",
+		"lua",
+		-- 'luadoc',
+		-- 'make',
+		"markdown",
+		"markdown_inline",
+		-- 'nginx',
+		'php',
+		-- "python",
+		"query",
+		"regex",
+		-- 'rust',
+		-- 'scss',
+		-- 'svelte',
+		"sql",
+		-- 'templ',
+		"toml",
+		"tsv",
+		"typescript",
+		"vim",
+		"vimdoc",
+		-- 'xml',
+		"yaml",
+		-- 'zig',
+	}
+
+	require("nvim-treesitter").install(parsers)
+
+	vim.api.nvim_create_autocmd("FileType", {
+		callback = function(args)
+			local buf, filetype = args.buf, args.match
+
+			local language = vim.treesitter.language.get_lang(filetype)
+			if not language then
+				return
+			end
+
+			-- check if parser exists and load it
+			if not vim.treesitter.language.add(language) then
+				return
+			end
+
+			-- enables syntax highlighting and other treesitter features
+			vim.treesitter.start(buf, language)
+
+			-- enables treesitter based indentation
+			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		end,
+	})
+end
+
+return M
