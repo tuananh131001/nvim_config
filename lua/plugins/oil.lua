024478950e6be816f6947a7619c31f3997758551
Anh Nguyen <anh.nt@raksul.com>
2026-05-04 15:22:15 +0700

    Bring back oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,38 +1,38 @@
 return {
-	-- {
-	-- 	"barrettruth/canola.nvim",
-	-- 	event = "VeryLazy",
-	-- 	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-	-- 	config = function(_, opts)
-	-- 		require("oil").setup(opts)
-	-- 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-	-- 		local oil = require("oil")
-	-- 		oil.setup({
-	-- 			keymaps = {
-	-- 				-- create a new mapping, gs, to search and replace in the current directory
-	-- 				gs = {
-	-- 					callback = function()
-	-- 						-- get the current directory
-	-- 						local prefills = { paths = oil.get_current_dir() }
-	--
-	-- 						local grug_far = require("grug-far")
-	-- 						-- instance check
-	-- 						if not grug_far.has_instance("explorer") then
-	-- 							grug_far.open({
-	-- 								instanceName = "explorer",
-	-- 								prefills = prefills,
-	-- 								staticTitle = "Find and Replace from Explorer",
-	-- 							})
-	-- 						else
-	-- 							grug_far.get_instance("explorer"):open()
-	-- 							-- updating the prefills without clearing the search and other fields
-	-- 							grug_far.get_instance("explorer"):update_input_values(prefills, false)
-	-- 						end
-	-- 					end,
-	-- 					desc = "oil: Search in directory",
-	-- 				},
-	-- 			},
-	-- 		})
-	-- 	end,
-	-- }
+	{
+		"barrettruth/canola.nvim",
+		event = "VeryLazy",
+		dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+		config = function(_, opts)
+			require("oil").setup(opts)
+			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+			local oil = require("oil")
+			oil.setup({
+				keymaps = {
+					-- create a new mapping, gs, to search and replace in the current directory
+					gs = {
+						callback = function()
+							-- get the current directory
+							local prefills = { paths = oil.get_current_dir() }
+
+							local grug_far = require("grug-far")
+							-- instance check
+							if not grug_far.has_instance("explorer") then
+								grug_far.open({
+									instanceName = "explorer",
+									prefills = prefills,
+									staticTitle = "Find and Replace from Explorer",
+								})
+							else
+								grug_far.get_instance("explorer"):open()
+								-- updating the prefills without clearing the search and other fields
+								grug_far.get_instance("explorer"):update_input_values(prefills, false)
+							end
+						end,
+						desc = "oil: Search in directory",
+					},
+				},
+			})
+		end,
+	}
 }
